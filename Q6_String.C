bc5f71468620f70df7ae5b5ecfd435f64e13fc64
csannrose7-blip <csannrose7@gmail.com>
2026-08-27 08:42:45 +0530

    Implement string concatenation
    Concatenates two strings input by the user and prints the result.

---
+++ Q6_String.C
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    char str1[100], str2[100];
+    int i, j, len=0;
+    scanf("%s %s", str1, str2);
+    for(i=0; str1[i]!='\0'; i++) len++;
+    for(j=0; str2[j]!='\0'; j++){
+        str1[len+j]=str2[j];
+    }
+    str1[len+j]='\0';
+    printf("%s", str1);
+    return 0;
+}
