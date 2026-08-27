033a43585bc5455f786fd9f50d47af9ce7af31ac
csannrose7-blip <csannrose7@gmail.com>
2026-08-27 08:40:32 +0530

    Implement vowel and consonant counting in C

---
+++ Q4_String.C
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main(){
+    char str[100];
+    int i, vowels=0, cons=0;
+    scanf("%s", str);
+    for(i=0; str[i]!='\0'; i++){
+        if(str[i]=='a' || str[i]=='e' || str[i]=='i' || str[i]=='o' || str[i]=='u' ||
+           str[i]=='A' || str[i]=='E' || str[i]=='I' || str[i]=='O' || str[i]=='U')
+            vowels++;
+        else
+            cons++;
+    }
+    printf("Vowels=%d Cons=%d", vowels, cons);
+    return 0;
+}
