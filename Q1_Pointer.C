8a801aeb74cef691b88a8f6e9a709be603f0f601
csannrose7-blip <csannrose7@gmail.com>
2026-08-29 07:39:22 +0530

    Add Q1_Pointer.C to demonstrate pointer usage

---
+++ Q1_Pointer.C
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main() {
+    int a = 10;
+    int *p;
+    p = &a;
+    printf("Value of a = %d\n", a);
+    printf("Address of a = %p\n", &a);
+    printf("Value of p (address) = %p\n", p);
+    printf("Value at *p = %d\n", *p);
+    return 0;
+}
